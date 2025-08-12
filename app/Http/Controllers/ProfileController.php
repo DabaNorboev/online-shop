c927d2bbd118c40b9d0eb1b72504fe1bab4d3af0
Daba Norboev <nored5t@ya.ru>
2025-08-12 09:22:03 +0700

    refactor: split UserController into AuthController and ProfileController

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -6,54 +6,11 @@ use App\Http\Requests\UpdatePasswordRequest;
 use App\Http\Requests\UpdateProfileRequest;
 use App\Models\Order;
 use App\Models\User;
-
-use App\Http\Requests\LoginRequest;
-use App\Http\Requests\SignUpRequest;
-
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
-class UserController
+class ProfileController extends Controller
 {
-    public function getSignUp()
-    {
-        return view('signUp');
-    }
-
-    public function signUp(SignUpRequest $request)
-    {
-        User::query()->create([
-            'first_name' => $request['first_name'],
-            'last_name' => $request['last_name'],
-            'email' => $request['email'],
-            'password' => Hash::make($request['password']),
-        ]);
-
-        return redirect()->route('login.form');
-    }
-
-    public function getLogin()
-    {
-        return view('login');
-    }
-
-    public function login(LoginRequest $request)
-    {
-        Auth::attempt([
-            'email' => $request->get('email'),
-            'password' => $request->get('password')
-        ]);
-
-        return redirect()->route('main');
-    }
-
-    public function logout()
-    {
-        Auth::logout();
-
-        return redirect()->route('login');
-    }
-
     public function getProfile()
     {
         $user = User::query()->find(Auth::id());
--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,27 @@
 <?php
 
-use App\Http\Controllers\UserController;
+
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/signup', [UserController::class, 'getSignUp'])->name('signup.form');
-Route::post('/signup', [UserController::class, 'signUp'])->name('signup');
+Route::get('/signup', [AuthController::class, 'getSignUp'])->name('signup.form');
+Route::post('/signup', [AuthController::class, 'signUp'])->name('signup');
 
-Route::get('/login', [UserController::class, 'getLogin'])->name('login.form');
-Route::post('/login', [UserController::class, 'login'])->name('login');
-Route::get('/logout', [UserController::class, 'logout'])->name('logout')->middleware('auth');
+Route::get('/login', [AuthController::class, 'getLogin'])->name('login.form');
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+Route::get('/logout', [AuthController::class, 'logout'])->name('logout')->middleware('auth');
 
 Route::view('/about' , 'about')->name('about')->middleware('auth');
 
-Route::get('/profile', [UserController::class, 'getProfile'])->name('profile')->middleware('auth');
-Route::put('/profile/update', [UserController::class, 'updateProfile'])->name('profile.update')->middleware('auth');
-Route::put('/profile/password', [UserController::class, 'updatePassword'])->name('profile.update.password')->middleware('auth');
+Route::get('/profile', [ProfileController::class, 'getProfile'])->name('profile')->middleware('auth');
+Route::put('/profile/update', [ProfileController::class, 'updateProfile'])->name('profile.update')->middleware('auth');
+Route::put('/profile/password', [ProfileController::class, 'updatePassword'])->name('profile.update.password')->middleware('auth');
 
 Route::get('/main', [ProductController::class, 'getMain'])->name('main')->middleware('auth');
 Route::get('/catalog/{categoryName?}', [ProductController::class, 'getCatalog'])->name('catalog')->middleware('auth');
